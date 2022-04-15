6d9f2ac9805908a58dd4649294f512e5c90070e0
deepaktimsina <deepakupdhaya@gmail.com>
2022-04-15 22:49:48 +0530

    Table Structure

---
+++ table.sql
@@ -0,0 +1,4 @@
+create table adut_emp(
+user_name varchar2(25),time_stamp date,id number(5),old_last_name varchar2(50),
+new_last_name varchar2(50),old_title varchar2(25),new_title varchar2(20),old_salary number(10),new_salary number(10));
+/
